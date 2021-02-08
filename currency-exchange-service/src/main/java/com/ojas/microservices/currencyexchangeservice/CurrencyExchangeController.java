1e0485df0cf0e928ab3f292ce5cdaefa8476dd32
venkateshpagadala143 <73212348+venkateshpagadala143@users.noreply.github.com>
2021-02-08 17:47:39 +0530

    Add files via upload

---
+++ currency-exchange-service/src/main/java/com/ojas/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,26 @@
+package com.ojas.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CurrencyExchangeController {
+	@Autowired
+	private Environment environment;
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}") // where {from} and {to} are path variable
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) 
+																									
+	{
+		ExchangeValue exchangeValue =  new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		return exchangeValue;
+	}
+}
